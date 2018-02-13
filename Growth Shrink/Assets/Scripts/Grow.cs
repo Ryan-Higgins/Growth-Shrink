388a61843094c564f9be28dfc407b6970f069cf6
Ryan Higgins <c16381886@mydit.ie>
2018-02-13 15:49:59 +0000

    Grown and Shrink code done.

---
+++ Growth Shrink/Assets/Scripts/Grow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grow : MonoBehaviour {
+
+	Transform player;
+	public float growthSpeed;
+	public float growthLimit;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//grows the player to a limit set from the inspector
+		if (player.localScale.x <= growthLimit) {
+			player.transform.localScale += new Vector3 (growthSpeed * Time.deltaTime, growthSpeed * Time.deltaTime, 0);
+		}
+	}
+}
---
+++ Growth Shrink/Assets/Scripts/Shrink.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrink : MonoBehaviour {
+
+	Transform player;
+	Transform spawn;
+
+	// Use this for initialization
+	void Start () {
+		//This finds the players and the spawns transforms
+		player = GameObject.Find ("Player").GetComponent<Transform> ();
+		spawn = GameObject.Find ("Spawn").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			//resets the player's scale and position
+			player.transform.localScale = new Vector3 (1, 1, 1);
+			player.transform.position = new Vector3 (spawn.position.x, spawn.position.y, spawn.position.z);
+		}
+	}
+}
