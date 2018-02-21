81e1202466f2823888188b09deac77d0dd205d36
Ryan Higgins <c16381886@mydit.ie>
2018-02-21 17:46:05 +0000

    Placeable spawn points working

--- Growth Shrink/Assets/Scripts/Spawn.cs
+++ Growth Shrink/Assets/Scripts/Spawn.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawn : MonoBehaviour {
+public class Spawn : MonoBehaviour
+{
 	public GameObject spawnPointPrefab;
-	Vector3 spawnPoint;
+	public Vector3 spawnPoint;
+	public bool isPlaced;
+	public GameObject clone;
+	Grow myGrow;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		isPlaced = false;
+		myGrow = GetComponent<Grow> ();
 	}
 
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
 		if (Input.GetButtonDown ("Fire1")) {
-			spawnPoint = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
-			Instantiate (spawnPointPrefab,spawnPoint,gameObject.transform.rotation);
+			if (!isPlaced) {
+				if (transform.localScale.x > 1) {
+					spawnPoint = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+					Instantiate (spawnPointPrefab, spawnPoint, gameObject.transform.rotation);
+					isPlaced = true;
+					gameObject.transform.localScale -= new Vector3 (myGrow.growthIncrease, myGrow.growthIncrease, 0);
+					clone = GameObject.Find ("Spawn Point(Clone)");
+				}
+			}
 		}
 	}
 }
