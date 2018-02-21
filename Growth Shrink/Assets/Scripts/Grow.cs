11033aa25225921869b25d509274726b87b78faf
Ryan Higgins <c16381886@mydit.ie>
2018-02-21 15:44:42 +0000

    Growing with collectibles

--- Growth Shrink/Assets/Scripts/Grow.cs
+++ Growth Shrink/Assets/Scripts/Grow.cs
@@ -4,24 +4,39 @@ using UnityEngine;
 
 public class Grow : MonoBehaviour {
 
-	Transform player;
-	public float growthSpeed;
+	public float growthIncrease;
 	public float growthLimit;
 	Move move;
 
 	// Use this for initialization
 	void Start () {
-		player = GameObject.Find ("Player").GetComponent<Transform> ();
-		move = GameObject.Find ("Player").GetComponent<Move> ();
+		move = GetComponent<Move> ();
 
+		//Ensuring values always have a default if not set in editor
+		if (growthIncrease == 0) {
+			growthIncrease = 1;
+		}
+
+		if (growthLimit == 0) {
+			growthLimit = 5;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//grows the player to a limit set from the inspector
-		if (player.localScale.x <= growthLimit) {
-			player.transform.localScale += new Vector3 (growthSpeed * Time.deltaTime, growthSpeed * Time.deltaTime, 0);
-			move.speed += growthSpeed;
+
+	}
+
+	void OnTriggerEnter2D(Collider2D cube) {
+		//Cube will destroy itself even if the player doesn't grow
+		if (cube.tag == "Grow") {
+			Destroy (cube.gameObject);
+			//checks the player is below the limit(if we want one) and grows them if they are below
+			if (transform.localScale.x <= growthLimit && transform.localScale.y <= growthLimit) {
+				transform.localScale += new Vector3(growthIncrease,growthIncrease,0);
+				//Adds speed each time so that the player can actually keep moving
+				move.speed += 50;
+			}
 		}
 	}
 }
