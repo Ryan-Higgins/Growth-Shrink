33eb2d4709b85c5435a0a93a6b3bb8ad71999cc4
Ryan Higgins <c16381886@mydit.ie>
2018-02-21 15:47:15 +0000

    Movement with controller and spawn points code started ( without growing with collectibles)

---
+++ Growth Shrink/Assets/Scripts/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+	public GameObject spawnPointPrefab;
+	Vector3 spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetButtonDown ("Fire1")) {
+			spawnPoint = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+			Instantiate (spawnPointPrefab,spawnPoint,gameObject.transform.rotation);
+		}
+	}
+}
