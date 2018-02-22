5af774e2cb74a37236ccba3a50b9b8c39e3bcc17
Ryan Higgins <c16381886@mydit.ie>
2018-02-22 10:57:59 +0000

    Prototype Playtest, without art

--- Growth Shrink/Assets/Scripts/Death.cs
+++ Growth Shrink/Assets/Scripts/Death.cs
@@ -6,11 +6,13 @@ public class Death : MonoBehaviour {
 
 	Spawn mySpawnPoint;
 	Transform levelSpawn;
+	Move myMove;
 
 	// Use this for initialization
 	void Start () {
 		mySpawnPoint = GetComponent<Spawn> ();
 		levelSpawn = GameObject.Find ("Spawn").GetComponent<Transform> ();
+		myMove = GetComponent<Move> ();
 	}
 	
 	// Update is called once per frame
@@ -18,10 +20,16 @@ public class Death : MonoBehaviour {
 		if (Input.GetButtonDown ("Fire2")) {
 			if (mySpawnPoint.isPlaced) {
 				transform.position = mySpawnPoint.spawnPoint;
+				transform.localScale = new Vector3 (1, 1, 1);
 				mySpawnPoint.isPlaced = false;
 				mySpawnPoint.clone.SetActive (false);
+				myMove.speed = 100;
+				myMove.jumpDivider = 5;
 			} else if (!mySpawnPoint.isPlaced) {
 				transform.position = levelSpawn.position;
+				transform.localScale = new Vector3 (1, 1, 1);
+				myMove.speed = 100;
+				myMove.jumpDivider = 5;
 			}
 		}
 	}
