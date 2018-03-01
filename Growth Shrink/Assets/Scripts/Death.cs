5d4d37ba1b863a45a122364203f3ce02159cb53c
Ryan Higgins <c16381886@mydit.ie>
2018-03-01 16:15:54 +0000

    2 Checkpoints working, and basic physics added to them

--- Growth Shrink/Assets/Scripts/Death.cs
+++ Growth Shrink/Assets/Scripts/Death.cs
@@ -2,32 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Death : MonoBehaviour {
+public class Death : MonoBehaviour
+{
 
 	Spawn mySpawnPoint;
-	Transform levelSpawn;
 	Move myMove;
+	bool spawnAtTwo;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		mySpawnPoint = GetComponent<Spawn> ();
-		levelSpawn = GameObject.Find ("Spawn").GetComponent<Transform> ();
 		myMove = GetComponent<Move> ();
+		spawnAtTwo = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetAxis ("Spawn") == 1) {
-			if (mySpawnPoint.firstPlaced) {
-				transform.position = mySpawnPoint.spawnPoint1;
-				mySpawnPoint.firstPlaced = false;
-				mySpawnPoint.clone1.SetActive (false);
-			} /*else if (!mySpawnPoint.isPlaced) {
-				transform.position = levelSpawn.position;
-				transform.localScale = new Vector3 (1, 1, 1);
-				myMove.speed = 100;
-				myMove.jumpDivider = 5;
-			}*/
+	void Update ()
+	{
+		if (Input.GetAxis ("Spawn") == 1) {		
+			if (mySpawnPoint.clone1.activeInHierarchy) {
+				if (!spawnAtTwo) {
+					gameObject.transform.position = mySpawnPoint.spawnPoint1;
+					gameObject.transform.localScale = mySpawnPoint.clone1.transform.localScale;
+					myMove.speed = mySpawnPoint.speed1;
+					myMove.jumpDivider = mySpawnPoint.jump1;
+					mySpawnPoint.firstPlaced = false;	
+					mySpawnPoint.clone1.SetActive (false);
+				}
+			} else if (mySpawnPoint.clone2.activeInHierarchy) {
+				if (spawnAtTwo) {
+					gameObject.transform.position = mySpawnPoint.spawnPoint2;
+					gameObject.transform.localScale = mySpawnPoint.clone2.transform.localScale;
+					myMove.speed = mySpawnPoint.speed2;
+					myMove.jumpDivider = mySpawnPoint.jump2;
+					mySpawnPoint.clone2.SetActive (false);
+				}
+			}
+		} else {
+			if (mySpawnPoint.clone1.activeInHierarchy) {
+				spawnAtTwo = false;
+			} else {
+				spawnAtTwo = true;
+			}
 		}
 	}
 }
--- Growth Shrink/Assets/Scripts/Spawn.cs
+++ Growth Shrink/Assets/Scripts/Spawn.cs
@@ -8,15 +8,26 @@ public class Spawn : MonoBehaviour
 	public Vector3 spawnPoint1;
 	public Vector3 spawnPoint2;
 	public bool firstPlaced;
+	public bool canPlaceSecond;
+	public bool secondPlaced;
 	public GameObject clone1;
 	public GameObject clone2;
+	public float speed1;
+	public float speed2;
+	public int jump1;
+	public int jump2;
+
 	Grow myGrow;
+	Move myMove;
 
 	// Use this for initialization
 	void Start ()
 	{
 		firstPlaced = false;
+		myMove = GetComponent<Move> ();
 		myGrow = GetComponent<Grow> ();
+		canPlaceSecond = false;
+		secondPlaced = false;
 	}
 
 	// Update is called once per frame
@@ -27,17 +38,34 @@ public class Spawn : MonoBehaviour
 				if (!firstPlaced) {
 					spawnPoint1 = new Vector3 (transform.position.x, transform.position.y, transform.position.z - 1);
 					Instantiate (spawnPointPrefab, spawnPoint1, gameObject.transform.rotation);
-					firstPlaced = true;
-					gameObject.transform.localScale -= new Vector3 (myGrow.growthIncrease, myGrow.growthIncrease, 0);
 					clone1 = GameObject.Find ("Spawn Point(Clone)");
 					clone1.name = "First";
-				} else if (firstPlaced) {
+					clone1.transform.localScale = gameObject.transform.localScale;
+					speed1 = myMove.speed;
+					jump1 = myMove.jumpDivider;
+					firstPlaced = true;
+					gameObject.transform.localScale -= new Vector3 (myGrow.growthIncrease, myGrow.growthIncrease, 0);
+					myMove.speed -= 200;
+					myMove.jumpDivider -= 5;
+				} else if (canPlaceSecond) {
 					spawnPoint2 = new Vector3 (transform.position.x, transform.position.y, transform.position.z - 1);
 					Instantiate (spawnPointPrefab, spawnPoint2, gameObject.transform.rotation);
-					gameObject.transform.localScale -= new Vector3 (myGrow.growthIncrease, myGrow.growthIncrease, 0);
 					clone2 = GameObject.Find ("Spawn Point(Clone)");
 					clone2.name = "Second";
+					clone2.transform.localScale = gameObject.transform.localScale;
+					speed2 = myMove.speed;
+					jump2 = myMove.jumpDivider;
+					secondPlaced = true;
+					gameObject.transform.localScale -= new Vector3 (myGrow.growthIncrease, myGrow.growthIncrease, 0);
+					myMove.speed -= 200;
+					myMove.jumpDivider -= 5;
 				}
+			}
+		} else {
+			if (firstPlaced) {
+				canPlaceSecond = true;
+			} else if (!firstPlaced) {
+				canPlaceSecond = false;
 			}
 		}
 	}
