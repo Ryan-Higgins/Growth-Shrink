15757fe695395f504b0544d66cb9755cd0f7b66b
Ryan Higgins <c16381886@mydit.ie>
2018-04-19 16:58:53 +0100

    Weighted Platform added and Crumbling platforms now respawn.

---
+++ Growth Shrink/Assets/Scripts/WeightPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightPlatform : MonoBehaviour {
+
+	public Vector3 startPosition;
+	bool playerOn;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = gameObject.transform.position; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!playerOn) {
+			transform.position = Vector3.Lerp (gameObject.transform.position, startPosition, 0.1f);
+		}
+
+	}
+
+	void OnCollisionExit2D(Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			playerOn = false;	
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			playerOn = true;
+		}
+	}
+}
