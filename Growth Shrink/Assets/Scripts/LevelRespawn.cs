929cd7b02661c80585f89c663cc06ce779a3377e
Ryan Higgins <c16381886@mydit.ie>
2018-04-27 14:16:15 +0100

    Pre-pull commit

---
+++ Growth Shrink/Assets/Scripts/LevelRespawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelRespawn : MonoBehaviour {
+
+	Scene scene;
+
+	// Use this for initialization
+	void Start () {
+		scene = SceneManager.GetActiveScene ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.R)) {
+			SceneManager.LoadScene (scene.name);
+		}
+	}
+}
