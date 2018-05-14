5a753ebbdd6fdaff1f78d7452b5891cc60f68ca1
TomTwinkerbell <36972477+TomTwinkerbell@users.noreply.github.com>
2018-05-14 20:55:39 +0100

    Progress

---
+++ Growth Shrink/Assets/Scripts/Button2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button2 : MonoBehaviour
+{
+
+    public GameObject thingToClose;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            thingToClose.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            thingToClose.SetActive(false);
+        }
+    }
+}
